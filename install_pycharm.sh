a876789f4c389c3bf27ffe3459d2c9f2166dc943
Ramon Wijnands <rayman747@hotmail.com>
2017-11-29 23:02:07 +0100

    Update pycharm installer ppa

--- install_pycharm.sh
+++ install_pycharm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:mystic-mirage/pycharm
+wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu zesty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+
 sudo apt update
 sudo apt install pycharm
 
