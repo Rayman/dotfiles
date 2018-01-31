8df41b6b26ecf8840cc86c91af87d3e5ac62bf03
Ramon Wijnands <rayman747@hotmail.com>
2018-01-31 12:07:50 +0100

    Install curl before vim-plug

--- install_vim-plug.sh
+++ install_vim-plug.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+hash curl 2> /dev/null | sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
