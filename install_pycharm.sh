ca229d23593f0effc8c695a19c2dd444d0f5c225
Ramon Wijnands <rayman747@hotmail.com>
2019-05-20 10:39:04 +0200

    Update installation of pycharm & sublime

--- install_pycharm.sh
+++ install_pycharm.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu zesty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
-
-sudo apt update
-sudo apt install pycharm
-
+snap install pycharm-community --classic
