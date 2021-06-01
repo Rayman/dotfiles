5845178eca4bac9453f0ba775867d1a14e3a51a0
Ramon Wijnands <rayman747@hotmail.com>
2021-06-01 17:02:28 +0200

    Fix install_minimum to work from any directory

--- install_minimum.sh
+++ install_minimum.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-xargs --arg-file=apt-minimum.keepers sudo apt install
+xargs --arg-file="$DIR/apt-minimum.keepers" sudo apt install
 
